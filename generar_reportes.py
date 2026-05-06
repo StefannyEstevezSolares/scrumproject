551379e9d96f0af432e76d353169f377cad035b4
Randolh Estuardo <estuardotecun@gmail.com>
2026-05-06 02:25:25 -0600

    feat: ✨ implement final products report with interactive search
    - Created productos_finales function to display a rich table of products.
    - Integrated real-time search by name and manufacturing date.
    - Added input validation and automatic view reset when no results are found.

--- generar_reportes.py
+++ generar_reportes.py
@@ -3,6 +3,8 @@ from rich.table import Table
 from rich import box
 from rich.text import Text
 import os
+from db import ARCHIVOS, buscar
+
 
 def limpiar_pantalla():
     """Limpia la pantalla de la consola."""
@@ -42,3 +44,44 @@ def generar_tabla(titulo, encabezados, datos, alineaciones=None):
         tabla.add_row(*map(str, fila))
 
     console.print(tabla)
+
+
+def productos_finales():
+    """Genera un reporte de productos finales y busqueda."""
+    productos = {}
+    while True:
+        limpiar_pantalla()
+        if not productos:
+            productos = buscar(ARCHIVOS["productos_finales"], parcial=True, nombre="", fecha_fabricacion="")
+            if not productos:
+                limpiar_pantalla()
+                print("\nNo hay productos finales registrados.")
+                input("Presiona Enter para continuar...\n")
+                return
+
+        titulo = "Reporte de Productos Finales"
+        encabezados = ["ID", "Nombre", "Descripción", "Precio de Venta (Q)", "Cantidad en Stock", "Fecha de Fabricación"]
+        alineaciones = ["center", "left", "left", "right", "center", "center"]
+        filas = [[id, p["nombre"], p["descripcion"], f'Q {p["precio_de_venta"]}', p["cantidad_en_stock"], p["fecha_fabricacion"]] for id, p in productos.items()]
+
+        generar_tabla(titulo, encabezados, filas, alineaciones)
+
+        print ("\nPuedes buscar un producto por Nombre o Fecha de Fabricación.")
+        buscar_input = input("Ingresa el término de búsqueda (o ingresa -Volver para regresar): ").strip().lower()
+
+        if buscar_input == "":
+            limpiar_pantalla()
+            print("\nLa búsqueda no puede estar vacía.")
+            input("Presiona Enter para continuar...\n")
+            productos = {}
+            continue
+        if buscar_input == "-volver":
+            return
+        
+        productos = buscar(ARCHIVOS["productos_finales"], parcial=True, nombre=buscar_input, fecha_fabricacion=buscar_input)
+
+        if not productos:
+            limpiar_pantalla()
+            print("\nNo se encontraron productos que coincidan con la búsqueda.")
+            input("Presiona Enter para continuar...\n")
+            productos = {}
